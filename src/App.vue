8fee7c6c4519c631d06e1d00186131ca55e1daf0
Chlowus <chlsmi040904@gmail.com>
2024-09-10 16:33:40 +0200

    files created

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,24 @@
     <router-link to="/about">Admin</router-link>
   </nav>
   <router-view/>
+  <FooterComp class=""/>
 </template>
+<script>
+// import NavBarComp from '@/components/NavBarComp.vue'
+import FooterComp from '@/components/FooterComp.vue'
 
+// import HomeView from '@/views/HomeView.vue'
+export default{
+  components:{
+    // NavBarComp,
+    FooterComp
+  }
+}
+</script>
 <style>
+
+@import url('https://fonts.googleapis.com/css2?family=Anton&family=Oswald:wght@200..700&display=swap');
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/FooterComp.vue
@@ -0,0 +1,115 @@
+<template>
+    <footer class="sticky-bottom">
+        <img src="https://chlowus.github.io/Nodejs.pictures/Capstone/footer.png" alt="#" class="footer-image">
+        <div class="logo">
+            <img src="https://chlowus.github.io/Nodejs.pictures/Capstone/Rubins%20LOGO.png" alt="#" class="logo-image">
+        </div>
+        <div class="fter-nav">
+
+            <router-link
+              class="nav-link"
+              :class="{ active: activeLink === 'product' }"
+              to="/product"
+              @click="setActive('product')"
+              >Product</router-link
+            >
+
+            <router-link
+              class="nav-link"
+              :class="{ active: activeLink === 'product' }"
+              to="/product"
+              @click="setActive('product')"
+              >Product</router-link
+            >
+
+            <router-link
+              class="nav-link"
+              :class="{ active: activeLink === 'product' }"
+              to="/product"
+              @click="setActive('product')"
+              >Product</router-link
+            >
+
+        </div>
+        <p class="c">&copy; {{year()}} Company, Inc</p>
+    </footer>
+</template>
+
+<script>
+export default {
+    methods:{
+        year(){
+            return new Date().getFullYear();
+        }
+    }
+}
+</script>
+
+<style scoped>
+footer {
+    position: relative; 
+    background-color: #000; 
+    width: 100.41vw;
+    height: 25vw;
+    margin-left: -2vw;
+    margin-bottom: 0; 
+}
+
+.footer-image {
+    position: absolute; 
+    top: -12vw;
+    left: 0;
+    width: 100%; 
+    height: 90%; 
+    object-fit: cover; 
+}
+
+body {
+    margin: 0; 
+    padding: 0;
+}
+
+.logo {
+    position: absolute;
+    left: 2vw; 
+    top: 65%; 
+    transform: translateY(-50%);
+}
+
+/* Adjust the size of the logo here */
+.logo-image {
+    width: 15vw; /* Adjust width as needed */
+    height: auto; /* Keeps the aspect ratio */
+}
+
+.fter-nav {
+    /* Add styles for the container div */
+    display: flex;
+    flex-wrap: wrap;
+    margin-top: 50px;
+    margin-left: 20vw;
+    gap: 4vw;
+    padding: 20px;
+  }
+  
+  .nav-link {
+    /* Style the router links */
+    color: #fff; /* Change the text color */
+    text-decoration: none; /* Remove underline */
+    margin: 0vw; /* Add some margin between links */
+    transition: color 0.2s ease; /* Add a transition effect */
+    margin-top: 14vw;
+  }
+  
+  .nav-link:hover {
+    /* Style the hover state */
+    color: #ccc; /* Change the text color on hover */
+  }
+  
+  .nav-link.active {
+    /* Style the active state */
+    color: #fff; /* Change the text color when active */
+    font-weight: bold; /* Make the text bold when active */
+  }
+
+</style>
